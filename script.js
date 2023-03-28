663ac7e738e99c7884ea2e8b7ba12223ee8853e0
nikolaas7 <119924275+nikolaas7@users.noreply.github.com>
2023-03-28 09:38:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let kakis = 'toms'
-
-console.log(kakis)
-
-kakis = 'jerijs'
-
-console.log(kakis)
-const plusins = document.querySelector('.js-plus');
-
-console.log(plusins)
+const coolButton = document.querySelector('.js-cool');
+const dontButton = document.querySelector('.js-dont');
+coolButton.addEventListener ('mouseover', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+})
